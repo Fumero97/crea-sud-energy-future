c2eba1e1993b556d7300836a2a285aaf95f00482
Fumero97 <vittario97@protonmail.com>
2026-05-04 02:49:47 +0200

    Fix: correzione errore sintassi Footer.tsx (div non chiuso)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -59,6 +59,7 @@ const Footer = () => {
               </p>
             </div>
           </div>
+        </div>
         <div className="pt-12 border-t border-white/10 flex flex-col md:flex-row justify-between gap-6">
           <p className="cs-small text-white/50">
             © 2026 CREA-SUD. Tutti i diritti riservati.
